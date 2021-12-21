69194ee934c1165c9264940b926040e8443bdfa9
Daniela Melo <ultradan@protonmail.com>
2021-12-20 23:20:29 -0500

    updating Post model dependencies

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Requests\StorePostRequest;
+use Illuminate\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
 }
